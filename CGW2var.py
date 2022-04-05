9f2030e4325825cb491bc990cf736c2e5a66b4f4
Dima Paryshkov <dima.paryshkov45@mail.ru>
2022-04-05 13:25:31 +0700

    feat: interface for DES + RSA

--- CGW2var.py
+++ CGW2var.py
@@ -1,9 +1,11 @@
-from email import charset
-import random
-from datetime import datetime
-from random import randint
 import PySimpleGUI as sg
 from Crypto.Util import number
+import base32hex
+import hashlib
+from Crypto.Cipher import DES
+import SymmetricCipher
+import os
+import base64
 
 def gcd(a, b):
     while b != 0:
@@ -39,7 +41,7 @@ def generate_keypair(p, q):
     n = p * q
     phi = (p - 1) * (q - 1)
 
-    for e in range(3, phi):
+    for e in range(((p-10)*(q-10)), phi):
         g = gcd(e, phi)
         d = multiplicative_inverse(e, phi)
         if g == 1 and e != d:
@@ -60,7 +62,6 @@ def encrypt(pk, plaintext):
         cipher.append(pow(ord(char), key, n))
     return cipher
 
-
 def decrypt(pk, ciphertext):
     key, n = pk
     plain = ""
@@ -68,16 +69,82 @@ def decrypt(pk, ciphertext):
         plain += chr(pow(char, key, n))
     return plain
 
+layout =  [
+                [sg.T('p\t'), sg.In(key='p', size=(70, 1))],
+                [sg.T('q\t'), sg.In(key='q', size=(70, 1))],
+                [sg.T('n\t'), sg.In(key='n', size=(70, 1))],
+                [sg.T('e\t'), sg.In(key='e', size=(70, 1))],
+                [sg.T('d\t'), sg.In(key='d', size=(70, 1))],
+                [sg.Text('_'  * 100, size=(30, 1)), sg.T('Initial data'), sg.Text('_'  * 100, size=(29, 1))], 
+                [sg.T('Key\t'), sg.In(key='Key', size=(70, 1))],
+                [sg.T('IV\t'), sg.In(key='IV', size=(70, 1))],
+                [sg.Push(), sg.Button('Generate new key and initialization vector'), sg.Button('Encrypt')],
+                [sg.Text('_'  * 100, size=(70, 1))],
+                [sg.Push(), sg.T('File content'), sg.Push()], 
+                [sg.MLine(key='File_content', size=(78,5))], 
+                [sg.Push(), sg.T('Encryption DES'), sg.Push(), sg.T('Encryption key RSA'), sg.Push()], 
+                [sg.MLine(key='DES', size=(37,5)), sg.Push(), sg.MLine(key='RSA', size=(37,5))],
+                [sg.Push(), sg.T('Decrypted file'), sg.Push()], 
+                [sg.MLine(key='DecFile', size=(78,5))]
+                ] 
 
-def main():
-    p = number.getPrime(128)
-    q = number.getPrime(128)
-    key_public, key_private = generate_keypair(p, q)
-    message = "pizdec"
-    encrypted_msg = encrypt(key_public, message)
-    plain = decrypt(key_private, encrypted_msg)
-    code = [ord(char) for char in message]
-    print(plain)
+window = sg.Window('Chiper RSA + DES', layout, grab_anywhere=True, finalize=True)
+
+key = SymmetricCipher.load_key()
+iv = SymmetricCipher.load_iv()
+window['Key'].Update(key)
+window['IV'].Update(base64.b64encode(iv).decode('cp1251'))
+
+while True:
+    event, values = window.read()  
+    if event == sg.WIN_CLOSED:       
+        break  
+
+    if event == 'Generate new key and initialization vector':
+        SymmetricCipher.write_key()
+        SymmetricCipher.write_iv()
+        
+        key = SymmetricCipher.load_key()
+        iv = SymmetricCipher.load_iv()
+        window['Key'].Update(key) 
+        window['IV'].Update(base64.b64encode(iv).decode('cp1251'))
+
+    if event == 'Encrypt':
+        p = number.getPrime(128)
+        q = number.getPrime(128)
+        key_public, key_private = generate_keypair(p, q)
+
+        window['p'].Update(p)
+        window['q'].Update(q)
+        window['e'].Update(key_public[0])
+        window['d'].Update(key_private[0])
+        window['n'].Update(key_public[1])
 
 
-main()
+        key = SymmetricCipher.load_key()
+        iv = SymmetricCipher.load_iv()
+
+        encrypted_msg = encrypt(key_public, base64.b64encode(key).decode('cp1251'))
+        plain = decrypt(key_private, encrypted_msg)
+
+        SC = SymmetricCipher.SymmetricCipher(os.path.basename("SymCh.txt"), key, iv)
+        a = open(os.path.basename("SymCh.txt"), 'r')
+        f = a.read()
+        a.close()
+        window['File_content'].Update(f)
+
+        window['DES'].Update(SC.encrypt_file(cipher='DES'))
+        a = open(os.path.basename("encryptDES.txt"), 'w')
+        a.write(SC.encrypt_file(cipher='DES'))
+        a.close()
+
+        window['RSA'].Update(encrypted_msg)
+        a = open(os.path.basename("encryptKeyRSA.txt"), 'w')
+        for item in encrypted_msg:
+            a.write(str(item))
+        a.close()
+
+        window['DecFile'].Update(SC.decrypt_file())
+        a = open(os.path.basename("decryptDES.txt"), 'w')
+        a.write(SC.decrypt_file())
+        a.close()
